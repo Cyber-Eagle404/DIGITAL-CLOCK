e38f64f5940e570b2507d90d51c7999a5ec4a3d8
Cyber-Eagle404 <dimsumbeneran@gmail.com>
2025-05-29 11:02:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -61,11 +61,11 @@ const motivasi = [
   "Bangkit, melangkah, Taklukan dunia!",
   "Setiap detik adalah kesempatan untuk menjadi lebih baik.",
   "Semangatmu adalah bahan bakar kemenanganmu!",
-  "Langit bukan batas, hanya titik awal!"
-  “Jangan ngeluh hidup susah kalau usahamu segitu-gitu aja.”,
-  “Orang lain sibuk cari peluang, kamu masih sibuk cari alasan.”,
-  “Masa depanmu nggak akan berubah kalau kamu masih nyantai terus.”,
-  “Rejeki emang di tangan Tuhan, tapi Tuhan nggak suka orang males.”,
+  "Langit bukan batas, hanya titik awal!",
+  "Jangan ngeluh hidup susah kalau usahamu segitu-gitu aja.",
+  "Orang lain sibuk cari peluang, kamu masih sibuk cari alasan.",
+  "Masa depanmu nggak akan berubah kalau kamu masih nyantai terus.",
+  "Rejeki emang di tangan Tuhan, tapi Tuhan nggak suka orang males.",
   "Duit nggak datang dari doa doang. Gerak, goblok.",
   "Mau kerja berat atau hidup berat? Pilih salah satu.",
   "Miskin karena lahir itu bukan salahmu. Tapi miskin karena males? 100% salahmu.",
@@ -75,8 +75,7 @@ const motivasi = [
   "Kalau kamu terus-terusan hidup berdasarkan mood, siap-siap miskin secara konsisten.",
   "Berani ngeluh, harusnya juga berani berubah. Kalau enggak, lo cuma pecundang yang berisik.",
   "Kalau kamu nggak bisa tahan susahnya belajar dan kerja, siapin diri buat tahan pahitnya penyesalan.",
-  "Kamu nggak harus semangat setiap hari. Tapi kamu harus tetap jalan, setiap hari.",
-
+  "Kamu nggak harus semangat setiap hari. Tapi kamu harus tetap jalan, setiap hari."
 ];
 
 let lastIndex = -1;
